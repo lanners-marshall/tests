dc027b98ba84a9d7068b3a7c7c056cb24e32a15a
lanners-marshall <lanners.marshall@gmail.com>
2019-04-02 12:37:56 -0700

    update

--- word_scramble_score.js
+++ word_scramble_score.js
@@ -1,133 +1,109 @@
-const rateScrambled = (scrambled_word, word) => {
-
-  //only a scrambled version of the word if it contains the same length and letters
-  //and not in the same order
+const rateScrambled = (sw, w) => {
 
   //NOT A SCRAMBLE
-  if (scrambled_word === word || (scrambled_word.length !== word.length)) {
-    console.log(`${scrambled_word} is not a scramble of ${word}`)
+  if (sw === w) {
+    console.log(`${sw} is not a scramble of ${w}`)
     return
   }
 
-  //NOT A SCRAMBLE 
-  let check_scramble = scrambled_word.split('').sort().join('')
-  let check_word = word.split('').sort().join('')
-  for (let i = 0; i < check_scramble.length; i++){
-    if (check_scramble[i] != check_word[i]){
-      console.log(`${scrambled_word} is not a scramble of ${word}`)
-      return
-    }
-  }
+  const vls = new Set(['A', 'E', 'I', 'O', 'U', 'Y'])
 
-  //helper function for checking if two or more consecutive letters are in the correct place
-  const poorHelper = (scrambled_word, word) => {
+  const conts = new Set(['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'])
 
-    let swLetters, wLetters
+  const combs = new Set(['AI', 'AY', 'EA', 'EE', 'EO', 'IO', 'OA', 'OO', 'OY', 'YA', 'YO', 'YU', 'BL', 'BR', 'CH', 'CK', 'CL', 'CR', 'DR', 'FL', 'FR', 'GH', 'GL', 'GR', 'KL', 'KR', 'KW', 'PF', 'PL', 'PR', 'SC', 'SCH', 'SCR', 'SH', 'SHR', 'SK', 'SL', 'SM', 'SN', 'SP', 'SQ', 'ST', 'SW', 'TH', 'THR', 'TR', 'TW', 'WH', 'WR', 'AA', 'BB', 'CC', 'DD', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL', 'MM', 'NN',  'PP', 'QQ', 'RR', 'SS', 'TT', 'UU', 'VV', 'WW', 'XX', 'YY', 'ZZ'])
 
-    //compare each two consecutive letters in each word
-    for (let i = 0; i < scrambled_word.length - 1; i++){
+  const check_real = (sw) => {
+    let two_conts, two_vowels, check_two, check_three
 
-      swLetters = scrambled_word.substr(i, 2)
-      wLetters = word.substr(i, 2)
+    for (let i = 0; i < sw.length; i++){
+
+      //if we have two vowels or two constants in a row
+      two_conts = conts.has(sw[i]) && conts.has(sw[i + 1])
+      two_vowels = vls.has(sw[i]) && vls.has(sw[i + 1])
+
+      //no three vowel combinations in set or higher
+      if (two_vowels && vls.has(sw[i + 2])){
+        return false
+      }
       
-      //if the two consecutive letters match return true
-      if (swLetters === wLetters) {
+      //no four constant combinations in set or higher
+      if (two_conts && conts.has(sw[i + 2]) && conts.has(sw[i + 3])) {
+        return false
+      }
+
+      //checking three constant combination
+      if (two_conts && conts.has(sw[i + 2])){
+        check_three = sw[i] + sw[i + 1] + sw[i + 2]
+        if (combs.has(check_three)){
+          {continue}
+        } else {
+          return false
+        }
+      }
+
+      //checking two constant or two vowel combination
+      if (two_conts || two_vowels){
+        check_two = sw[i] + sw[i + 1]
+        if (combs.has(check_two)){
+          {continue}
+        } else {
+          return false
+        }
+      }
+
+    }
+    return true
+  }
+  
+  //checking if two or more consecutive letters are in the correct place
+  const poorHelper = (sw, w) => {
+    for (let i = 0; i < sw.length - 1; i++){
+      if ((sw[i] + sw[i + 1]) === (w[i] + w[i + 1])){
         return true
       }
     }
     return false
-
   }
 
-  //if its hard will return true, else false
-  const hardHelper = (scrambled_word, word) => {
-    for (let i = 0; i < word.length; i++){
-      if (scrambled_word[i] === word[i]){
+  //checking if any consecutive letters are in the correct place
+  const hardHelper = (sw, w) => {
+    for (let i = 0; i < w.length; i++){
+      if (sw[i] === w[i]){
         return false
       }
     }
     return true
   }
-  
-  //helper function for checking if scrabled word looks real
-  const check_real = (scrambled) => {
-    
-    //using sets so that look up speed is in 0(1)
-    const vowels = new Set(['A', 'E', 'I', 'O', 'U', 'Y'])
-
-    const constants = new Set(['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'])
-
-    let combinations = new Set(['AI', 'AY', 'EA', 'EE', 'EO', 'IO', 'OA', 'OO', 'OY', 'YA', 'YO', 'YU', 'BL', 'BR', 'CH', 'CK', 'CL', 'CR', 'DR', 'FL', 'FR', 'GH', 'GL', 'GR', 'KL', 'KR', 'KW', 'PF', 'PL', 'PR', 'SC', 'SCH', 'SCR', 'SH', 'SHR', 'SK', 'SL', 'SM', 'SN', 'SP', 'SQ', 'ST', 'SW', 'TH', 'THR', 'TR', 'TW', 'WH', 'WR', 'AA', 'BB', 'CC', 'DD', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL', 'MM', 'NN',  'PP', 'QQ', 'RR', 'SS', 'TT', 'UU', 'VV', 'WW', 'XX', 'YY', 'ZZ'])
-
-    let two_constants, two_vowels
-
-    for (let i = 0; i < scrambled.length - 1; i++){
-
-      two_constants = constants.has(scrambled[i]) && constants.has(scrambled[i + 1])
-      two_vowels = vowels.has(scrambled[i]) && vowels.has(scrambled[i + 1])
-
-      if (two_constants || two_vowels) {
-
-        //no three vowel combinations in set or higher
-        if (two_vowels && vowels.has(scrambled[i + 2])){
-          return false
-        }
-
-        two_check = scrambled.substr(i, 2)
-
-
-        if (combinations.has(two_check)){
-          if (two_constants && constants.has(scrambled[i + 2]) && constants.has(scrambled[i + 3])){
-            //no four constant combinations in set or higher
-            return false
-          }
-          //if there is a three constant combination
-          if (two_constants && constants.has(scrambled[i + 2])){
-            three_check = scrambled.substr(i, 3)
-            if (combinations.has(three_check)){
-              {continue}
-            } else {
-              //the three constant combination in the set does not exist
-              return false
-            }
-          }
-        } else {
-          //it does not have the two constant or two vowel combination in the set
-          return false
-        }
-      }
-    }
-    //all conditions are met and word looks real
-    return true
-  }
 
   //these will evaluate to either true or false
-  const checkFirstLetter = scrambled_word[0] === word[0]
-  const checkTwoConsecutive = poorHelper(scrambled_word, word)
-  const is_hard = hardHelper(scrambled_word, word)
-  const is_real = check_real(scrambled_word)
+  const checkFirstLetter = sw[0] === w[0]
+  const checkTwoConsecutive = poorHelper(sw, w)
+  const is_hard = hardHelper(sw, w)
+  const is_real = check_real(sw)
 
   //POOR SCRAMBLE
   if ((checkFirstLetter || checkTwoConsecutive) && is_real === false){
-    console.log(`${scrambled_word} is a poor scramble of ${word}`)
+    console.log(`${sw} is a poor scramble of ${w}`)
     return
   }
 
   //HARD SCRAMBLE
   if (is_hard && is_real){
-    console.log(`${scrambled_word} is a hard scramble of ${word}`)
+    console.log(`${sw} is a hard scramble of ${w}`)
     return
   }
 
   //FAIR SCRAMBLE
-  console.log(`${scrambled_word} is a fair scramble of ${word}`)
+  console.log(`${sw} is a fair scramble of ${w}`)
 }
 
-//inputs scrambled word, word 
+//inputs scrambled w, w 
 rateScrambled('MAPS', 'SPAM')
 rateScrambled('RIONY', 'IRONY')
 rateScrambled('ONYRI', 'IRONY')
 rateScrambled('IRONY', 'IRONY')
 rateScrambled('INOYR', 'IRONY') 
 rateScrambled('IOYRN', 'IRONY')
+rateScrambled('IOYRN', 'IRONY')
+
 
